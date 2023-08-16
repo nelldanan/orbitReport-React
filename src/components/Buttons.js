5a4d9725c95a919cc40f964a4bfc48ae0cb2da70
nelldanan <eloperano1@gmail.com>
2023-08-16 08:08:47 -0500

    CompleteAllTestsPass

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -12,7 +12,7 @@ function Buttons({ filterByType, setSat, displaySats }) {
                </button>
             );
          })}
-         <button onClick={() => setSat(satData)}>Show All Satellites</button>
+         <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
    );
 }
